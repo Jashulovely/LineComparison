551c864906a46701d12ba4bea191707e2cb95e66
jashulovely <porsu.jaswanthkumar@gmail.com>
2023-10-02 19:56:09 +0530

    [Jashu]Add: Line equal comparison.

--- LineComparison/LineComparison/Length.cs
+++ LineComparison/LineComparison/Length.cs
@@ -10,17 +10,30 @@ namespace LineComparison
     {
         public void LengthInfo()
         {
-            Console.WriteLine("Enter the coordinates of the Line.");
+            int x = 0;
+            int y = 0;
+            Console.WriteLine("Enter end point coordinates of the Line 1.");
             Console.WriteLine("Enter x1 : ");
             int x1 = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter y1 : ");
             int y1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter end point coordinates of the Line 2.");
             Console.WriteLine("Enter x2 : ");
             int x2 = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter y2 : ");
             int y2 = int.Parse(Console.ReadLine());
-            double length = Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2));
-            Console.WriteLine("The lenth of the line is : " + length);
+            double lengthL1 = Math.Sqrt(Math.Pow((x-x1), 2) + Math.Pow((y-y1), 2));
+            Console.WriteLine("The lenth of the line is : " + lengthL1);
+            double lengthL2 = Math.Sqrt(Math.Pow((x - x2), 2) + Math.Pow((y - y2), 2));
+            Console.WriteLine("The lenth of the line is : " + lengthL2);
+            if (lengthL1 == lengthL2)
+            {
+                Console.WriteLine("Both lines are equal in length.");
+            }
+            else
+            {
+                Console.WriteLine("Both lines are not equal in length.");
+            }
         }
     }
 }
