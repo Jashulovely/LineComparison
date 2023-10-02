c4fd1fe881aa8cc351123e946605aa295595a52e
jashulovely <porsu.jaswanthkumar@gmail.com>
2023-10-02 20:01:17 +0530

    [Jashu]Add: Line comparison.

--- LineComparison/LineComparison/Length.cs
+++ LineComparison/LineComparison/Length.cs
@@ -30,10 +30,15 @@ namespace LineComparison
             {
                 Console.WriteLine("Both lines are equal in length.");
             }
+            else if (lengthL1 > lengthL2)
+            {
+                Console.WriteLine("Line 1 is greater than Line 2.");
+            }
             else
             {
-                Console.WriteLine("Both lines are not equal in length.");
+                Console.WriteLine("Line 2 is greater than Line 1.");
             }
+
         }
     }
 }
